d06e3dd6864d5395aa0ad985f65e0710c48ed84c
hophuocduy2610 <hophuocduy261020@gmail.com>
2022-09-30 23:51:37 +0700

    Done Cau2

---
+++ spring-jpa-test-practice/spring-jpa-test-practice/src/main/java/com/example/springjpatestpractice/repository/MayBayRepository.java
@@ -0,0 +1,12 @@
+package com.example.springjpatestpractice.repository;
+
+import com.example.springjpatestpractice.entity.MayBay;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MayBayRepository extends CrudRepository<MayBay, Integer> {
+    @Query("select mb.loai from MayBay mb where mb.tamBay > 10000")
+    List<String> findLoaiMayBayByTamBay();
+}
