c8ff84afaabf80c1f898909099be318ff453eb95
hophuocduy2610 <hophuocduy261020@gmail.com>
2022-10-05 19:00:41 +0700

    Finish Cau 5

--- spring-boot-api/spring-boot-api/src/main/java/com/example/springbootapi/Service/ChuyenBayRepository.java
+++ spring-boot-api/spring-boot-api/src/main/java/com/example/springbootapi/Service/ChuyenBayRepository.java
@@ -11,4 +11,6 @@ public interface ChuyenBayRepository extends CrudRepository<ChuyenBay, String> {
     List<ChuyenBay> findChuyenBayByGaDen(String gaDen);
     @Query("select cb from ChuyenBay cb where cb.doDai between 8000 and 10000")
     List<ChuyenBay> findChuyenBayByDoDai();
+    @Query("select cb from ChuyenBay cb where cb.gaDi = 'SGN' and cb.gaDen = 'BMV'")
+    List<ChuyenBay> findChuyenBayByGaDiVaGaDen();
 }
