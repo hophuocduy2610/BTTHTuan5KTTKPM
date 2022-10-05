9bbfbc3baf53f038f2037a407f6953392b7d8891
hophuocduy2610 <hophuocduy261020@gmail.com>
2022-10-05 18:04:10 +0700

    Create project

---
+++ spring-boot-api/spring-boot-api/src/main/java/com/example/springbootapi/Service/ChuyenBayRepository.java
@@ -0,0 +1,8 @@
+package com.example.springbootapi.Service;
+
+import com.example.springbootapi.Entity.ChuyenBay;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ChuyenBayRepository extends CrudRepository<ChuyenBay, String> {
+
+}
