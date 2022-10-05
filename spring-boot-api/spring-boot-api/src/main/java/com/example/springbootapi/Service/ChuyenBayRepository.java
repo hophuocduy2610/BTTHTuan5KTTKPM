79a1247ae43ab9d1fef3acc5218304e855c0cadc
hophuocduy2610 <hophuocduy261020@gmail.com>
2022-10-05 23:25:50 +0700

    Finish Cau 18

--- spring-boot-api/spring-boot-api/src/main/java/com/example/springbootapi/Service/ChuyenBayRepository.java
+++ spring-boot-api/spring-boot-api/src/main/java/com/example/springbootapi/Service/ChuyenBayRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ChuyenBayRepository extends CrudRepository<ChuyenBay, String> {
     @Query("select cb from ChuyenBay cb where cb.gaDen = ?1")
@@ -18,7 +19,8 @@ public interface ChuyenBayRepository extends CrudRepository<ChuyenBay, String> {
     @Query("select cb from ChuyenBay cb " +
             "where cb.doDai <= (select mb.tamBay from MayBay mb where mb.loai = ?1)")
     List<ChuyenBay> findChuyenBayByMayBayAirbusA320(String loaiMB);
-    //SELECT * FROM chuyenbay WHERE (GaDi, GaDen) IN (SELECT GaDen, GaDi FROM chuyenbay)
     @Query(value = "select * from chuyenbay cb where (cb.gaDi, cb.gaDen) in (select cb1.gaDen, cb1.gaDi from chuyenbay cb1)", nativeQuery = true)
     List<ChuyenBay> findCBDiTuGaADenBVeLaiA();
+    @Query(value = "select cb.gaDi, count(cb.maCB) as SoLuongChuyenBay from chuyenbay cb group by cb.gaDi", nativeQuery = true)
+    List<Map<String, Object>> countCBDiTuGaADenBVeLaiA();
 }
